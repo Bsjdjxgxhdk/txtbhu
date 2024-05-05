3b7141ec97a4427145cc5f0698abbca67285bfc3
Rock8032 <153748315+Rock8032@users.noreply.github.com>
2024-05-05 18:10:45 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "23537514"
 api_hash = "c5ec016af7d2cb637278b715f4f5a389"
-bot_token = "6719113215:AAHyQ3pBQooo2WHXR_iVkNu8YSEvTpjqwV0"
+bot_token = "7035167959:AAGUFiduMJg2uQhZDbTX8Oh_1n4d5ROKxT8"
